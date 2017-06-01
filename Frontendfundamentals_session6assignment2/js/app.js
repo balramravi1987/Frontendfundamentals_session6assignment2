ece753af308b538c536e5ee41d7778c76681ae03
balramravi1987 <balramravi1987@gmail.com>
2017-06-01 21:37:28 +0530

    Add files via upload

---
+++ Frontendfundamentals_session6assignment2/js/app.js
@@ -0,0 +1,10 @@
+var a=20,b=6,c=13;
+if (a>b  && a>c){
+console.log("A = " + a + " is greater");
+}
+else if (b>a  && b>c){
+console.log("B = " + b + " is greater");
+}
+else{
+console.log("C = " + c + " is greater");
+}
